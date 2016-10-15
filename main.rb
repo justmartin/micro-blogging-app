02e9dd1d6f9f39bbf769576a4d0701901c7dd646
justmartin <justin.lamar.martin@gmail.com>
2016-10-14 22:45:36 -0700

    created users db table and sample users

--- main.rb
+++ main.rb
@@ -2,8 +2,28 @@ require "bundler/setup"
 require "sinatra"
 require "sinatra/activerecord"
 require "./models"
+require "sinatra/flash"
 set :database, "sqlite3:micro_blogging_app.sqlite3"
+enable :sessions
+set :sessions, true
 
 get "/" do
 	erb :index
+end
+
+get "/home" do
+	erb :home
+end
+
+post "/sign-in" do
+	@user = User.where(email: params[:email], password: params[:password]).first
+
+	if @user.try(:password) == params[:password]
+		session[:user_id] = @user.id
+		flash[:notice] = "Sign in successful!"
+		redirect "/home"
+	else
+		flash[:alert] = "Sign in failed. If you don't have an account, Sign Up!"
+	end
+
 end
