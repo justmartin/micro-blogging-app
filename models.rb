e771210da86ce1d9e1707e421454080c69bda8a3
justmartin <justin.lamar.martin@gmail.com>
2016-10-19 10:46:23 -0700

    added methods on user model and post model

--- models.rb
+++ models.rb
@@ -1,7 +1,15 @@
 class User <ActiveRecord::Base
 	has_many :posts
+
+	def full_name
+		first_name + " " + last_name
+	end
 end
 
 class Post <ActiveRecord::Base
 	belongs_to :user
+
+	def custom_created_at
+		created_at.strftime("%D")
+	end
 end
